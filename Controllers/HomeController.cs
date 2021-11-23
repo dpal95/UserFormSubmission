fb50df01aecdd79eddff9c1eddf1471726703382
David Paluszkiewicz <dpaluszkiewicz@yahoo.co.uk>
2021-11-23 19:40:33 +0000

    intial set up for project, added sql to project, repos, services and connectionstring

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace UserFormSubmission.Controllers
 {
